5c73a6750fa8a9add706c2f2326937b2897d84f2
sli41 <sli41@ncsu.edu>
2017-03-04 15:12:40 -0500

    update solution of c1 week5

--- Algorithmic_Toolbox/week5/04_dynamic_programming_starter_files/primitive_calculator/PrimitiveCalculator.java
+++ Algorithmic_Toolbox/week5/04_dynamic_programming_starter_files/primitive_calculator/PrimitiveCalculator.java
@@ -1,17 +1,47 @@
 import java.util.*;
 
 public class PrimitiveCalculator {
+    private static final int INF = Integer.MAX_VALUE;
+
     private static List<Integer> optimal_sequence(int n) {
         List<Integer> sequence = new ArrayList<Integer>();
-        while (n >= 1) {
-            sequence.add(n);
-            if (n % 3 == 0) {
-                n /= 3;
-            } else if (n % 2 == 0) {
-                n /= 2;
-            } else {
-                n -= 1;
+        int[] counts = new int[n+1];
+        ////Arrays.fill(counts, INF);
+        // while (n >= 1) {
+        //     sequence.add(n);
+        //     if (n % 3 == 0) {
+        //         n /= 3;
+        //     } else if (n % 2 == 0) {
+        //         n /= 2;
+        //     } else {
+        //         n -= 1;
+        //     }
+        // }
+        // Collections.reverse(sequence);
+        for (int i=0; i<=n; i++){
+            if (i>1){
+                int t1, t2, t3;
+                t1 = counts[i-1]+1;
+                t2 = (i%2==0)? counts[i/2]+1: INF;
+                t3 = (i%3==0)? counts[i/3]+1: INF;
+                counts[i] = Math.min(t1, Math.min(t2, t3));
             }
+        }
+        // retrieve the solutions using backtracking
+        sequence.add(n);
+        int x = n;
+        while (x>1) {
+            int t1 = counts[x-1]+1;
+            int t2 = (x%2==0)? counts[x/2]+1: INF;
+            int t3 = (x%3==0)? counts[x/3]+1: INF;
+            if (t1<=t2 && t1<=t3){
+                x--;
+            } else if (t2<t1 && t2<t3){
+                x /= 2;
+            } else {
+                x /= 3;
+            }
+            sequence.add(x);
         }
         Collections.reverse(sequence);
         return sequence;
