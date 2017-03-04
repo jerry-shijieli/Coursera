95131eab5e9669d4600577c07265b56985a2589b
sli41 <sli41@ncsu.edu>
2017-03-04 17:02:36 -0500

    update solution to c1 week5
    This course is done!

--- Algorithmic_Toolbox/week5/04_dynamic_programming_starter_files/knapsack/Knapsack.java
+++ Algorithmic_Toolbox/week5/04_dynamic_programming_starter_files/knapsack/Knapsack.java
@@ -4,11 +4,24 @@ public class Knapsack {
     static int optimalWeight(int W, int[] w) {
         //write you code here
         int result = 0;
-        for (int i = 0; i < w.length; i++) {
-          if (result + w[i] <= W) {
-            result += w[i];
-          }
+        // for (int i = 0; i < w.length; i++) {
+        //   if (result + w[i] <= W) {
+        //     result += w[i];
+        //   }
+        // }
+        int[][] values = new int[w.length+1][W+1];
+        for (int i=0; i<=w.length; i++){
+            for (int j=0; j<=W; j++){
+                if (i>0 && j>0){
+                    values[i][j] = values[i-1][j];
+                    if (j>=w[i-1]){
+                        int val = values[i-1][j-w[i-1]] + w[i-1];
+                        values[i][j] = Math.max(val, values[i][j]);
+                    }
+                }
+            }
         }
+        result = values[w.length][W];
         return result;
     }
 
