d77b95e2c678961d449502fcf86b61c73a182318
sli41 <sli41@ncsu.edu>
2017-06-20 00:21:33 -0400

    c3 week5

--- Algorithms_on_Graphs/week5/11_spanning_trees_starter_files/clustering/Clustering.java
+++ Algorithms_on_Graphs/week5/11_spanning_trees_starter_files/clustering/Clustering.java
@@ -1,9 +1,97 @@
 import java.util.Scanner;
+import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Clustering {
+    private static class Edge {
+        int src;
+        int dest;
+        double weight;
+
+        Edge(int src, int dest, double weight){
+            this.src = src;
+            this.dest = dest;
+            this.weight = weight;
+        }
+    }
+
+    private static double distance(int src, int dest, int[] x, int[] y){
+        return Math.pow(x[dest]-x[src],2)+Math.pow(y[dest]-y[src], 2); // square of 2D distance
+    }
+
+    private static int findSet(int x, int[] parent){
+        if (x != parent[x])
+            x = parent[x];
+        return x;
+    }
+
+    private static void unionSet(int root1, int root2, int[] parent, int[] rank){
+        if (root1 == root2)
+            return;
+        int r1 = root1, r2=root2;
+        if (rank[r1] > rank[r2])
+            parent[r2] = r1;
+        else if (rank[r1] < rank[r2])
+            parent[r1] = r2;
+        else {
+            parent[r1] = r2;
+            rank[r2]++;
+        }
+    }
+
     private static double clustering(int[] x, int[] y, int k) {
         //write your code here
-        return -1.;
+        double inter_cluster_distance = Integer.MAX_VALUE;
+        int numOfPoints = x.length;
+        if (numOfPoints == 0) return 0;
+        // put all edges into mini heap
+        PriorityQueue<Edge> edge_queue = new PriorityQueue<Edge>(numOfPoints*(numOfPoints-1)/2, new Comparator<Edge>(){
+            @Override 
+            public int compare(Edge e1, Edge e2){
+                if (e1.weight > e2.weight)
+                    return 1;
+                else if (e1.weight < e2.weight)
+                    return -1;
+                else
+                    return 0;
+            }
+        });
+        for (int u=0; u<numOfPoints; u++){
+            for (int v=u+1; v<numOfPoints; v++){
+                edge_queue.offer(new Edge(u, v, distance(u, v, x, y)));
+            }
+        }
+        // initialize clusters (sets)
+        int[] parent = new int[numOfPoints];    
+        int[] rank = new int[numOfPoints];
+        for (int u=0; u<numOfPoints; u++){
+            parent[u] = u; // MakeSet
+            rank[u] = 0;
+        }
+        // adopt Kruskal algorithm to merge nodes until only k sets left
+        int setCount = numOfPoints;
+        while (setCount > k && !edge_queue.isEmpty()){
+            Edge tmp = edge_queue.poll();
+            int set1 = findSet(tmp.src, parent);
+            int set2 = findSet(tmp.dest, parent);
+            if (set1 != set2){
+                setCount--;
+                unionSet(set1, set2, parent, rank);
+            }
+        }
+        // find the smallest intercluster distance from remaining edges
+        while (!edge_queue.isEmpty()){
+            Edge tmp = edge_queue.poll();
+            int set1 = findSet(tmp.src, parent);
+            int set2 = findSet(tmp.dest, parent);
+            if (set1 != set2){
+                inter_cluster_distance = Math.sqrt(tmp.weight);
+                break;
+            }
+        }
+        
+        return inter_cluster_distance;
     }
 
     public static void main(String[] args) {
--- Algorithms_on_Graphs/week5/11_spanning_trees_starter_files/connecting_points/ConnectingPoints.java
+++ Algorithms_on_Graphs/week5/11_spanning_trees_starter_files/connecting_points/ConnectingPoints.java
@@ -1,9 +1,64 @@
 import java.util.Scanner;
 
 public class ConnectingPoints {
+    private static final double INF = Double.MAX_VALUE;
+
+    private static double weight(int src, int dest, int[] x, int[] y){
+        return Math.sqrt(Math.pow(x[dest]-x[src],2)+Math.pow(y[dest]-y[src],2));
+    }
+
+    private static boolean isQueueEmpty(boolean[] isInQueue){
+        for (boolean x: isInQueue)
+            if (x)
+                return false;
+        return true;
+    }
+
+    private static int extractMin(double[] cost, boolean[] isInQueue){
+        int minIndex = -1;
+        double minCost = INF;
+        for (int i=0; i<cost.length; i++){
+            if (isInQueue[i] && minCost>cost[i]){
+                minCost = cost[i];
+                minIndex = i;
+            }
+        }
+        if (minIndex != -1)
+            isInQueue[minIndex] = false;
+        return minIndex;
+    }
+
     private static double minimumDistance(int[] x, int[] y) {
         double result = 0.;
         //write your code here
+        int numOfPoints = x.length;
+        double[] cost = new double[numOfPoints];
+        int[] prev = new int[numOfPoints]; // parent in MST
+        boolean[] isInQueue = new boolean[numOfPoints]; // flag whether in Priority Queue measured by cost
+        for (int i=0; i<numOfPoints; i++){
+            cost[i] = INF;
+            prev[i] = -1;
+            isInQueue[i] = true;
+        }
+        cost[0] = 0;
+        // apply Prims algorithm to obtain MST
+        while (!isQueueEmpty(isInQueue)){
+            int v = extractMin(cost, isInQueue);
+            for (int z=0; z<numOfPoints; z++){
+                if (z!=v && isInQueue[z]){
+                    double w_vz = weight(v,z,x,y);
+                    if (cost[z]>w_vz){
+                        cost[z] = w_vz;
+                        prev[z] = v;
+                    }
+                }
+            }
+        }
+        // sum up distance among nodes in MST
+        for (int i=0; i<numOfPoints; i++){
+            result += cost[i];
+        }
+
         return result;
     }
 
