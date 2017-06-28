e935f2b984012dba0ff619663b648d9332ca7fb6
sli41 <sli41@ncsu.edu>
2017-06-28 00:31:47 -0400

    c4 week4 update solution

--- Algorithms_on_Strings/week4/04_Programming-Assignment-3/kmp/KnuthMorrisPratt.java
+++ Algorithms_on_Strings/week4/04_Programming-Assignment-3/kmp/KnuthMorrisPratt.java
@@ -23,12 +23,34 @@ public class KnuthMorrisPratt {
         }
     }
 
+    private int[] computePrefixFunction(String allText){
+        int[] result = new int[allText.length()];
+        int border = 0;
+        for (int i=1; i<allText.length(); i++){
+            while (border>0 && allText.charAt(i)!=allText.charAt(border)){
+                border = result[border-1];
+            }
+            if (allText.charAt(i) == allText.charAt(border))
+                border++;
+            else
+                border = 0;
+            result[i] = border;
+        }
+        return  result;
+    }
+
     // Find all the occurrences of the pattern in the text and return
     // a list of all positions in the text (starting from 0) where
     // the pattern starts in the text.
     public List<Integer> findPattern(String pattern, String text) {
         ArrayList<Integer> result = new ArrayList<Integer>();
         // Implement this function yourself
+        String unionString = pattern + '$' + text;
+        int[] prefixArray = computePrefixFunction(unionString);
+        for (int i=pattern.length()+1; i<unionString.length(); i++){
+            if (prefixArray[i] == pattern.length())
+                result.add(i-2*pattern.length());
+        }
         return result;
     }
 
